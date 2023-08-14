39be81feb2818677beb67ede4e374b96c644c385
Ricardo Pompilio <ricardopompiliodossantos@gmail.com>
2023-08-14 13:22:27 -0300

    TASK-H6T3 Adicionando e listando lista de compras

---
+++ src/models/shoppingList.ts
@@ -0,0 +1,32 @@
+import { InferSchemaType, Schema, model } from "mongoose";
+import { Product } from "./product";
+
+export type ShoppingListProducts = {
+  product: Product;
+  quantity: number;
+};
+
+const shoppingListSchema = new Schema(
+  {
+    storeId: { type: Schema.Types.ObjectId, ref: "Store", required: true },
+    creatorId: { type: Schema.Types.ObjectId, ref: "User", required: true },
+    products: [
+      {
+        product: {
+          type: Schema.Types.ObjectId,
+          ref: "Product",
+          required: true,
+        },
+        quantity: { type: Number, required: true },
+      },
+    ],
+    totalValue: { type: Number, required: true },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+type ShoppingList = InferSchemaType<typeof shoppingListSchema>;
+
+export default model<ShoppingList>("ShoppingList", shoppingListSchema);
